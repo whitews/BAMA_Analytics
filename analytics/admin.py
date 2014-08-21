1936fe75b7a786d2a97e796b9b05ee94724f883a
Scott White <whitews@gmail.com>
2014-08-21 11:25:25 -0400

    register models with admin site

--- analytics/admin.py
+++ analytics/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from guardian.admin import GuardedModelAdmin
+
+from analytics.models import *
+
+
+class ProjectAdmin(GuardedModelAdmin):
+    user_can_access_owned_objects_only = True
+    pass
+
+admin.site.register(Cohort)
+admin.site.register(Project)
+admin.site.register(Species)
+admin.site.register(Network)
+admin.site.register(Participant)
+admin.site.register(UploadEvent)
+admin.site.register(Notebook)
+admin.site.register(Analyte)
+admin.site.register(Conjugate)
+admin.site.register(Isotype)
+admin.site.register(Buffer)
+admin.site.register(SampleType)
+admin.site.register(DataPoint)
+admin.site.register(ProjectDataPoint)
